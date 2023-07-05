584312ddc3a69e6d44e8ba021ab68fc9ace5f830
YelyzavetaAndriienko <65223500+YelyzavetaAndriienko@users.noreply.github.com>
2023-07-05 16:12:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function changeLogo(image) {
+    if (image.src == "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"){
+        image.src = "images/oddity.jpg";
+    } else {
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png";
+    }
+}
